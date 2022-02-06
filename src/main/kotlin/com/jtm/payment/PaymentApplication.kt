14036009804a2a1cb4a791cefc00fa5ec2b8f1de
Joe Matthews <joetymatthews@gmail.com>
2022-02-06 22:18:13 +0000

    new: initial project files.

---
+++ src/main/kotlin/com/jtm/payment/PaymentApplication.kt
@@ -0,0 +1,13 @@
+package com.jtm.payment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class PaymentApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaymentApplication>(*args)
+}
---
+++ src/test/kotlin/com/jtm/payment/PaymentApplicationTestSuite.kt
@@ -0,0 +1,8 @@
+package com.jtm.payment
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(value = [])
+class PaymentApplicationTestSuite
